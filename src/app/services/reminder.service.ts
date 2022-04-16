50257905a9a8e645f2d6133219c4ec34311e7f52
MBuchalik <dev@martin-buchalik.de>
2022-04-16 13:03:28 +0200

    Add local notifications

--- src/app/services/reminder.service.ts
+++ src/app/services/reminder.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { LocalNotifications } from '@capacitor/local-notifications';
 import { Subject } from 'rxjs';
 
 const LOCALSTORAGE_KEY_REMINDER_ACTIVE = 'reminder-active';
@@ -33,10 +34,47 @@ export class ReminderService {
   private activateReminder(): void {
     window.localStorage.setItem(LOCALSTORAGE_KEY_REMINDER_ACTIVE, 'true');
     this.onReminderStatusChange.next(true);
+
+    void this.showLocalNotification();
   }
 
   private deactivateReminder(): void {
     window.localStorage.setItem(LOCALSTORAGE_KEY_REMINDER_ACTIVE, 'false');
     this.onReminderStatusChange.next(false);
+
+    void this.hideAllLocalNotifications();
+  }
+
+  private async showLocalNotification(): Promise<void> {
+    // Make sure there are no leftover notifications we did not hide for some reason.
+    await this.hideAllLocalNotifications();
+
+    let permissionStatus = await LocalNotifications.checkPermissions();
+    if (permissionStatus.display === 'prompt') {
+      permissionStatus = await LocalNotifications.requestPermissions();
+    }
+
+    if (permissionStatus.display !== 'granted') {
+      return;
+    }
+
+    await LocalNotifications.schedule({
+      notifications: [
+        {
+          title: 'Remember Something',
+          body: 'I should remind you of something...',
+          id: 1,
+
+          ongoing: true,
+          autoCancel: false,
+        },
+      ],
+    });
+  }
+
+  private async hideAllLocalNotifications(): Promise<void> {
+    const { notifications } = await LocalNotifications.getPending();
+
+    await LocalNotifications.cancel({ notifications: notifications });
   }
 }
